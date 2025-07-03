684fa69a416192416d33b542e59e04aea1e951d0
muhammadsamiullah <mailksamiullah607@gmail.com>
2025-07-03 17:32:16 +0500

    erts

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:sportzstar/helper/page_navigate.dart';
-import 'package:sportzstar/routing/routing_constrants.dart';
-
-import '../widgets/Layout/main_layout_widget.dart';
-
-import 'package:flutter/material.dart';
-
-class HomeScreen extends StatelessWidget {
-=======
 import 'package:sportzstar/config/palette.dart';
 import 'package:stylish_bottom_bar/stylish_bottom_bar.dart';
 
 class HomeScreen extends StatefulWidget {
->>>>>>> 00a8c8e7d205e0c005569570f0147ebfce6aa7be
   const HomeScreen({super.key});
 
   @override
@@ -34,100 +23,6 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Best Social App to Make\nNew Friends',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'With pipel you will find new friends from various\ncountries and regions of the world',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.grey,
-                ),
-              ),
-              const SizedBox(height: 40),
-              // Get Started Button
-              SizedBox(
-                width: double.infinity,
-                height: 55,
-                child: ElevatedButton(
-                  onPressed: () {
-                    pushNamedNavigate(context: context, pageName: signupScreenRoute);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: StadiumBorder(),
-                    padding: const EdgeInsets.all(0),
-                    backgroundColor: const Color.fromARGB(255, 223, 255, 43), // Start of gradient
-                  ).copyWith(
-                    backgroundColor: WidgetStateProperty.resolveWith<Color?>(
-                      (Set<WidgetState> states) {
-                        return null; // Use decoration instead
-                      },
-                    ),
-                    elevation: WidgetStateProperty.all(0),
-                  ),
-                  child: Ink(
-                    decoration: BoxDecoration(
-                      gradient: const LinearGradient(
-                        colors: [Color.fromARGB(255, 223, 255, 43), Color.fromARGB(255, 223, 255, 43)],
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight,
-                      ),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: Container(
-                      alignment: Alignment.center,
-                      child: const Text(
-                        'Get Started',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              // Login Button
-              SizedBox(
-                width: double.infinity,
-                height: 55,
-                child: OutlinedButton(
-                  onPressed: () {
-                    pushNamedNavigate(context: context, pageName: loginScreenRoute);
-                  },
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.black12),
-                  ),
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.black87,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-=======
       body: pages[selectedIndex],
       floatingActionButton: Container(
         width: 56.0,
@@ -139,7 +34,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   ? Palette.primaryGradient
                   : Palette.secondaryGradient,
           boxShadow: [BoxShadow(color: Colors.black26, blurRadius: 8)],
->>>>>>> 00a8c8e7d205e0c005569570f0147ebfce6aa7be
         ),
         child: FloatingActionButton(
           shape: CircleBorder(),
