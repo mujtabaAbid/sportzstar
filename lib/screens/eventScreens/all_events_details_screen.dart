88010b5552ff6c129383a6f270416e422fa6fa5a
muhammadsamiullah <mailksamiullah607@gmail.com>
2025-08-07 13:35:56 +0500

    dsw

--- lib/screens/eventScreens/all_events_details_screen.dart
+++ lib/screens/eventScreens/all_events_details_screen.dart
@@ -22,27 +22,27 @@ class _EventDetailScreenState extends State<EventDetailScreen> {
         .join(' ');
   }
 
-// void joinEvent() async {
-//     try {
-//       final response = await Provider.of<EventProvider>(
-//         context,
-//         listen: false,
-//       ).joinEventFun(eventId: event.eventId.toString());
-//       final responseData = json.decode(response.body);
-//       if (response.statusCode == 201) {
-//         print('joinevent response success -------->$responseData');
-//         alertNotification(
-//           context: context,
-//           message: responseData['message'],
-//           messageType: AlertMessageType.success,
-//         );
-//       } else {
-//         print('joinevent response error  -------->$responseData');
-//       }
-//     } catch (e) {
-//       print('joinevent response error  e-------->$e');
-//     }
-//   }
+  // void joinEvent() async {
+  //     try {
+  //       final response = await Provider.of<EventProvider>(
+  //         context,
+  //         listen: false,
+  //       ).joinEventFun(eventId: event.eventId.toString());
+  //       final responseData = json.decode(response.body);
+  //       if (response.statusCode == 201) {
+  //         print('joinevent response success -------->$responseData');
+  //         alertNotification(
+  //           context: context,
+  //           message: responseData['message'],
+  //           messageType: AlertMessageType.success,
+  //         );
+  //       } else {
+  //         print('joinevent response error  -------->$responseData');
+  //       }
+  //     } catch (e) {
+  //       print('joinevent response error  e-------->$e');
+  //     }
+  //   }
   @override
   Widget build(BuildContext context) {
     return MainLayoutWidget(
@@ -54,72 +54,144 @@ class _EventDetailScreenState extends State<EventDetailScreen> {
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Event image
-            widget.event.pictures.isNotEmpty
-                ? Image.network(
-                  widget.event.pictures.first,
-                  width: double.infinity,
-                  height: 350,
-                  fit: BoxFit.cover,
-                )
-                : Container(
-                  width: double.infinity,
-                  height: 200,
-                  color: Colors.grey,
-                  child: const Center(child: Icon(Icons.image, size: 50)),
-                ),
-            const SizedBox(height: 16),
-            Text(
-              capitalizeEachWord(widget.event.title),
-              style: const TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              "Hosted by: ${capitalizeEachWord(widget.event.hostName)}",
-              style: const TextStyle(fontSize: 18, color: Colors.white),
-            ),
-
-            const SizedBox(height: 8),
-            Text(
-              "Date: ${widget.event.date}",
-              style: const TextStyle(fontSize: 16, color: Colors.white),
-            ),
-            const SizedBox(height: 4),
-            Text(
-              "Time: ${widget.event.time}",
-              style: const TextStyle(fontSize: 16, color: Colors.white),
-            ),
-            const SizedBox(height: 4),
-            Row(
+        child: Card(
+          color: const Color.fromARGB(29, 255, 255, 255),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Icon(Icons.location_on_outlined, size: 16,  color: Colors.white),
-                const SizedBox(width: 5),
-                Expanded(
-                  child: Text(
-                    widget.event.location,
-                    style: const TextStyle(fontSize: 16, color: Colors.white),
+                // Event image
+                widget.event.pictures.isNotEmpty
+                    ? ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        topRight: Radius.circular(12),
+                      ),
+                      child: Image.network(
+                        widget.event.pictures.first,
+                        width: double.infinity,
+                        height: 350,
+                        fit: BoxFit.cover,
+                        loadingBuilder: (
+                          BuildContext context,
+                          Widget child,
+                          ImageChunkEvent? loadingProgress,
+                        ) {
+                          if (loadingProgress == null) {
+                            return child; // Image is fully loaded
+                          }
+                          return Container(
+                            width: double.infinity,
+                            height: 350,
+                            color: Colors.black12,
+                            child: const Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          );
+                        },
+                        errorBuilder: (context, error, stackTrace) {
+                          return Container(
+                            width: double.infinity,
+                            height: 200,
+                            color: Colors.grey,
+                            child: const Center(
+                              child: Icon(Icons.broken_image, size: 50),
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                    : Container(
+                      width: double.infinity,
+                      height: 200,
+                      color: Colors.grey,
+                      child: const Center(child: Icon(Icons.image, size: 50)),
+                    ),
+
+                const SizedBox(height: 16),
+                Text(
+                  capitalizeEachWord(widget.event.title),
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
                   ),
                 ),
+                const SizedBox(height: 8),
+                Text(
+                  "Hosted by: ${capitalizeEachWord(widget.event.hostName)}",
+                  style: const TextStyle(fontSize: 18, color: Colors.grey),
+                ),
+
+                const SizedBox(height: 8),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.calendar_today,
+                      size: 18,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(width: 6),
+                    Text(
+                      "Date: ${widget.event.date}",
+                      style: const TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 4),
+                Row(
+                  children: [
+                    const Icon(Icons.access_time, size: 18, color: Colors.grey),
+                    const SizedBox(width: 6),
+                    Text(
+                      "Time: ${widget.event.time}",
+                      style: const TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 4),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.location_on_outlined,
+                      size: 16,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(width: 5),
+                    Expanded(
+                      child: Text(
+                        capitalizeEachWord(widget.event.location),
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  "Description",
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  capitalizeEachWord(widget.event.description),
+                  style: const TextStyle(fontSize: 16, color: Colors.grey),
+                ),
+                SizedBox(height: 12),
               ],
             ),
-            const SizedBox(height: 16),
-            Text(
-              "Description",
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 4),
-            Text(
-              widget.event.description,
-              style: const TextStyle(fontSize: 16, color: Colors.white),
-            ),
-          ],
+          ),
         ),
       ),
       bottomNavigationBar: Container(
